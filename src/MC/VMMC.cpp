46fed019dbb3fa597f6126ce36510a20e0aa632c
Oliver Henrich <oliver.henrich@strath.ac.uk>
2025-07-28 12:47:50 +0100

    Moved vmmc object initialisation into init(), made random seed available

--- src/MC/VMMC.cpp
+++ src/MC/VMMC.cpp
@@ -212,7 +212,7 @@ namespace vmmc
 #ifdef ISOTROPIC
         std::cout << " (isotropic)";
 #endif
-        std::cout << ".\nseed\t" << rng.getSeed() << '\n';
+//        std::cout << ".\nseed\t" << rng.getSeed() << '\n';
         // Print version info.
 #ifdef COMMIT
         std::cout << "commit\t" << COMMIT << '\n';
